1e540612092d071fe9546d15615c8b59fb32f728
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-12-03 06:52:20 +0900

    Fix: make it install vim 8

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@
 # build-essential
 sudo apt-get install build-essential
 
-# Vim
-sudo apt-get install vim
+# Vim 8
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+# Vundle.vim
+git clone http://github.com/VundleVim/Vundle.Vim.git ~/.vim/bundle/Vundle.vim
 
 # gdb-peda
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
-
-# Vundle.vim
-git clone http://github.com/VundleVim/Vundle.Vim.git ~/.vim/bundle/Vundle.vim
 
 # Synbolic linc
 ln ./.vimrc ~/.vimrc
