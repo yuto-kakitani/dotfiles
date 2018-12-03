b462af4ab6d9741aee12bdb937c9a2af92f6bef4
垣谷悠斗 <hr.8714hmg@icloud.com>
2018-12-03 09:14:55 +0900

    Bug fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Synbolic linc
+ln ./.vimrc ~/.vimrc
+ln ./.bash_profile ~/.bash_profile
+ln ./.gdbinit ~/.gdbinit
+
 # build-essential
 sudo apt-get install build-essential
 
@@ -10,15 +15,11 @@ sudo apt install vim
 
 # Vundle.vim
 git clone http://github.com/VundleVim/Vundle.Vim.git ~/.vim/bundle/Vundle.vim
+sudo vim -c "BundleInstall" -c "q" -c "q"
 
 # gdb-peda
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 
-# Synbolic linc
-ln ./.vimrc ~/.vimrc
-ln ./.bash_profile ~/.bash_profile
-ln ./.gdbinit ~/.gdbinit
 
-sudo vim -c "BundleInstall" -c "q" -c "q"
 source ~./bash_profile
